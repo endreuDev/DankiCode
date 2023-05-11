8cb7db3a98445e28f95f834f0a67d86b439b572f
Andrew Oliveira <andrewcanto@hotmail.com>
2023-05-11 13:39:51 -0400

    Initial module complete

---
+++ eclipse-workspace/teste_curso_games/src/teste_curso_games/ElfInimigo.java
@@ -0,0 +1,12 @@
+package teste_curso_games;
+
+public class ElfInimigo extends Inimigo {
+
+	public ElfInimigo(int vida) {
+		super(vida);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
--- eclipse-workspace/teste_curso_games/src/teste_curso_games/Game.java
+++ eclipse-workspace/teste_curso_games/src/teste_curso_games/Game.java
@@ -1,19 +1,29 @@
 package teste_curso_games;
+import java.util.ArrayList;
 
 public class Game {
 	
 	public static void main(String[] args) {
+		
 		Player player = new Player();
+		ArrayList<Inimigo> inimigos = new ArrayList<Inimigo>();
 		
-		System.out.println(player.vida);
+		inimigos.add(new ElfInimigo(100));
+		inimigos.add(new ElfInimigo(100));
+		inimigos.add(new ElfInimigo(100));
+		inimigos.add(new ElfInimigo(100));
+		inimigos.add(new ElfInimigo(100));
+
+		for(int i = 0; i < inimigos.size(); i++) {
+			Inimigo inimigoLocal = inimigos.get(i);
+			
+			if(i == 1) {
+				inimigoLocal.tomarDano();
+				System.out.println(inimigoLocal.vida);
+			}else {
+				System.out.println(inimigoLocal.vida);
+			}
+		}
 		
-		player.perderVida();
-		player.perderVida();
-		
-		System.out.println(player.vida);
-		
-		player.ganharVida();
-		
-		System.out.println(player.vida);
 	}
 }
